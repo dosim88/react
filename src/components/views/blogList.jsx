87953876520b64a690601cf757947d2183998b2b
Dmitriy Simanovskyi <dmitriy.simanovskyi@gmail.com>
2017-07-25 12:14:49 +0300

    Lesson 6: Added pieChart.

--- src/components/views/blogList.jsx
+++ src/components/views/blogList.jsx
@@ -2,21 +2,33 @@ import React, { PropTypes } from 'react';
 import _ from 'lodash';
 
 import BlogItem from 'components/views/blogItem';
+import PieChart from 'components/ui/pieChart';
 
-const BlogList = ({ posts, handleLike }) => {
+const BlogList = ({ posts, handleLike }) => (
+  <div className="container">
+    <div className="row">
+      <div className="col-md-6">
+        {
+          _.map(posts, (post) => (
+            <BlogItem
+              key={post.id}
+              post={post}
+              //handleLike={ () => handleLike(post.id) }
+            />
+          ))
+        }
+      </div>
 
-  return <div>
-    {
-      _.map(posts, (post) => (
-        <BlogItem
-          key={post.id}
-          post={post}
-          //handleLike={ () => handleLike(post.id) }
+      <div className="col-md-6">
+        <PieChart columns={
+          posts.map(
+            post => [ post.text, post.meta.likesCount ]
+          )}
         />
-      ))
-    }
-  </div>;
-};
+      </div>
+    </div>
+  </div>
+);
 
 BlogList.propTypes = {
   posts: PropTypes.array,
