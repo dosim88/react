f1efbbc4291b9d84a0dbba6893d11d5d4c6bff35
Dmitriy Simanovskyi <dmitriy.simanovskyi@gmail.com>
2017-08-22 21:11:36 +0300

    Lesson 10: Added browser tests.

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -1,11 +1,51 @@
 import nightmare from 'nightmare';
+import { APP_ROOT } from 'constants/app';
 
-describe('When visit the homepage', () => {
-  it('welcomes the user', async () => {
+describe('Browser tests', () => {
+  it('Home page', async () => {
     let page = nightmare().goto('http:localhost:3000');
-
     let text = await page.evaluate(() => (document.body.textContent)).end();
 
     expect(text).toContain("React blog");
-  })
+  });
+
+  it('About page', async () => {
+    let page = nightmare()
+      .goto(APP_ROOT)
+      .click(`a[href="/about"]`);
+
+    let text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).toContain('Описание блога');
+  });
+
+  it('Contacts page', async () => {
+    let page = nightmare()
+      .goto(APP_ROOT)
+      .click(`a[href="/contacts"]`);
+
+    let text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).toContain('Обратная связь');
+  });
+
+  it('Create-post page', async () => {
+    let page = nightmare()
+      .goto(APP_ROOT)
+      .click(`a[href="/create-post"]`);
+
+    let text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).toContain('Создание поста');
+  });
+
+  it('Edit post', async () => {
+    let page = nightmare()
+      .goto(APP_ROOT)
+      .click('a[href="/posts/1/edit"]');
+
+    let text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).toContain('post 2 (Predators)');
+  });
 });
